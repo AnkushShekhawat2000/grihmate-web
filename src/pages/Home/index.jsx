89c1c8b184d337ba0df1fdee5f20f6d01dcd842a
Ankush Shekhawat <ankush75056@gmail.com>
2025-08-12 17:39:39 +0530

    one page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,9 @@ import Header from "../../component/common/Header"
 import "./style.css"
 import HeroSection from "../../component/common/HeroSection"
 import FeaturedProperties from "../../component/FeaturedProperties"
-
+import ClientFeedback from "../../component/ClientFeedback"
+import ContactSection from "../../component/ContactUs/index"
+import City from "../../component/City/index"
 const Home = () => {
     return (
         <div className="home-page">
@@ -11,6 +13,9 @@ const Home = () => {
             <main>
                 <HeroSection/>
                 <FeaturedProperties/>
+                <ClientFeedback/>
+                <ContactSection/>
+                <City/>
             </main>
 
         </div>
