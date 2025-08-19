5fd672090c1b080023b6ae3c204116b2817f9716
Ankush Shekhawat <ankush75056@gmail.com>
2025-08-19 11:27:28 +0530

    test2

--- src/pages/Rent/components/RentProperties/index.jsx
+++ src/pages/Rent/components/RentProperties/index.jsx
@@ -1,13 +1,13 @@
-import React from "react"
-import "./style.css"
+// import React from "react"
+// import "./style.css"
 
-const RentProperties = () => {
-    return (
-        <div className="rent-properties">
-            <div className="filter-sidebar"></div>
-            <div className="cards-section"> </div>
-        </div>
-    )
-}
+// const RentProperties = () => {
+//     return (
+//         <div className="rent-properties">
+//             <div className="filter-sidebar"></div>
+//             <div className="cards-s ection"> </div>
+//         </div>
+//     )
+// }
 
-export default RentProperties
+// export default RentProperties
